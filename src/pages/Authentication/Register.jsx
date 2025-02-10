342affc1498930ac38942d4dcdca2a8fe19b09e3
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-02-11 01:45:37 +0600

    update register page

--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -64,7 +64,9 @@ const Registration = () => {
       <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-green-50 rounded-lg shadow-lg  lg:max-w-4xl '>
         <div className='w-full px-6 py-8 md:px-8 lg:w-1/2'>
               <div className='flex justify-center mx-auto'>
-                      <img className=' h-12 ' src={logo} alt='' />
+              <Link to={'/'}>
+              <img className=' h-12 ' src={logo} alt='' />
+            </Link>
                     </div>
 
           <p className='mt-3 text-xl text-center text-gray-600 '>
